3470bdeff91ba17cb5943f4afcb848b88b2d31bd
ryanbmarx <ryanbmarx@gmail.com>
2017-02-17 14:34:00 -0600

    Refine legend design.

--- js/src/draw-legend.js
+++ js/src/draw-legend.js
@@ -7,65 +7,71 @@ import * as d3 from "d3";
 function drawLegend(rScale){
 	const	allPasses = 300,
 			completePasses = 100,
-			lineOffset = 15,
+			width = rScale(allPasses),
+			lineOffset = 40,
 			textOffset = 5,
 			scaledAllPass = rScale(allPasses) / 2, // circle radius/not diameter
 			scaledCompletePass = rScale(completePasses) / 2; // circle radius/not diameter
 	
 	const legend = d3.select('#legend__container')
 		.append('svg')
-		.attr('height', 100)
-		.attr('width', '100%');
+		.attr('height', 210)
+		.attr('width', width);
 	
-
 	// ADD CIRCLES
 	legend.append('circle')
 		.classed('legend__circle', true)
 		.classed('legend__circle--all', true)
 		.attr('r', scaledAllPass)
 		.attr('cx',scaledAllPass)
-		.attr('cy',scaledAllPass);
+		.attr('cy',scaledAllPass + lineOffset);
 
 	legend.append('circle')
 		.classed('legend__circle--complete', true)
 		.classed('legend__circle', true)
 		.attr('r', scaledCompletePass)
 		.attr('cx',scaledAllPass)
-		.attr('cy',scaledAllPass);
+		.attr('cy',scaledAllPass + lineOffset);
 
 	// ADD LABELS
 	legend.append('text')
 		.classed('legend__label', true)
 		.text(`${allPasses} attempts`)
-		.attr('text-anchor', 'left')
-		.attr('x',scaledAllPass * 2 + lineOffset + textOffset)
-		.attr('y',scaledAllPass / 2)
-		.attr('dy', '.35em');
+		.attr('x',width/2)
+		.attr('y',0)
+		.attr('text-anchor', 'middle')
+		.attr('dy', '1em');
 
 	legend.append('text')
-		.text(`${completePasses} completions`)
+		.text(completePasses)
 		.classed('legend__label', true)
-		.attr('text-anchor', 'left')
-		.attr('x',scaledAllPass * 2 + lineOffset + textOffset)
-		.attr('y',scaledAllPass)
-		.attr('dy', '.35em');
+		.attr('text-anchor', 'middle')
+		.attr('x',scaledAllPass)
+		.attr('y',(scaledAllPass + lineOffset) * 2);
+
+	legend.append('text')
+		.text(`completions`)
+		.classed('legend__label', true)
+		.attr('text-anchor', 'middle')
+		.attr('x',scaledAllPass)
+		.attr('y',(scaledAllPass + lineOffset + 10.5) * 2 );
 
 	// ADD LINES
 	legend.append('line')
 		.classed('legend__line', true)
 		.classed('legend__line--all', true)
 		.attr('x1',scaledAllPass)
-		.attr('y1',scaledAllPass / 2 )
-		.attr('x2',scaledAllPass * 2 + lineOffset)
-		.attr('y2',scaledAllPass / 2)
+		.attr('y1',22 )
+		.attr('x2',scaledAllPass)
+		.attr('y2',63)
 
 	legend.append('line')
 		.classed('legend__line', true)
 		.classed('legend__line--complete', true)
 		.attr('x1',scaledAllPass)
-		.attr('y1',scaledAllPass)
-		.attr('x2',scaledAllPass * 2 + lineOffset)
-		.attr('y2',scaledAllPass)
+		.attr('y1',scaledAllPass + 50)
+		.attr('x2',scaledAllPass)
+		.attr('y2',(scaledAllPass * 2) + 60)
 		
 }
 
