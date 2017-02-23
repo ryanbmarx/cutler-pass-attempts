2a608c14241e5f2603c83d2b84aa73db79525a64
ryanbmarx <ryanbmarx@gmail.com>
2017-02-23 17:44:18 -0600

    The bubble resize on devices and the legend was scaling all wonky. New design forces everything to the center of the container, so everything remains aligned.

--- js/src/draw-legend.js
+++ js/src/draw-legend.js
@@ -5,73 +5,50 @@ import * as d3 from "d3";
 // This styles the chart legend
 
 function drawLegend(rScale){
-	const	allPasses = 300,
+	const	allPasses = 200,
 			completePasses = 100,
 			width = rScale(allPasses),
-			lineOffset = 40,
-			textOffset = 5,
-			scaledAllPass = rScale(allPasses) / 2, // circle radius/not diameter
-			scaledCompletePass = rScale(completePasses) / 2; // circle radius/not diameter
+			// lineOffset = 40,
+			// textOffset = 5,
+			scaledAllPass = width, // circle radius/not diameter
+			scaledCompletePass = rScale(completePasses); // circle radius/not diameter
 	
 	const legend = d3.select('#legend__container')
-		.append('svg')
-		.attr('height', 210)
-		.attr('width', width);
+
+	// ADD things top to bottom
+	legend.append('p')
+		.classed('legend__label', true)
+		.text(`${allPasses} attempts`);
+
+	const circles = legend
+		.append('div')
+		.classed('legend__circles-container', true)
+		.attr('style', `height:${width}px;width:${width}px;`);
+
+	legend.append('p')
+		.text(`${completePasses} completions`)
+		.classed('legend__label', true)
+
 	
 	// ADD CIRCLES
-	legend.append('circle')
+	circles.append('div')
 		.classed('legend__circle', true)
 		.classed('legend__circle--all', true)
-		.attr('r', scaledAllPass)
-		.attr('cx',scaledAllPass)
-		.attr('cy',scaledAllPass + lineOffset);
+		.attr('style', `height:${scaledAllPass}px;width:${scaledAllPass}px;`);
 
-	legend.append('circle')
+	circles.append('div')
 		.classed('legend__circle--complete', true)
 		.classed('legend__circle', true)
-		.attr('r', scaledCompletePass)
-		.attr('cx',scaledAllPass)
-		.attr('cy',scaledAllPass + lineOffset);
-
-	// ADD LABELS
-	legend.append('text')
-		.classed('legend__label', true)
-		.text(`${allPasses} attempts`)
-		.attr('x',width/2)
-		.attr('y',0)
-		.attr('text-anchor', 'middle')
-		.attr('dy', '1em');
-
-	legend.append('text')
-		.text(completePasses)
-		.classed('legend__label', true)
-		.attr('text-anchor', 'middle')
-		.attr('x',scaledAllPass)
-		.attr('y',(scaledAllPass + lineOffset) * 2);
-
-	legend.append('text')
-		.text(`completions`)
-		.classed('legend__label', true)
-		.attr('text-anchor', 'middle')
-		.attr('x',scaledAllPass)
-		.attr('y',(scaledAllPass + lineOffset + 10.5) * 2 );
+		.attr('style', `height:${scaledCompletePass}px;width:${scaledCompletePass}px;margin:${scaledCompletePass / -2}px 0 0 ${scaledCompletePass / -2}px`);
 
 	// ADD LINES
-	legend.append('line')
+	legend.append('div')
 		.classed('legend__line', true)
 		.classed('legend__line--all', true)
-		.attr('x1',scaledAllPass)
-		.attr('y1',22 )
-		.attr('x2',scaledAllPass)
-		.attr('y2',63)
 
-	legend.append('line')
+	legend.append('div')
 		.classed('legend__line', true)
 		.classed('legend__line--complete', true)
-		.attr('x1',scaledAllPass)
-		.attr('y1',scaledAllPass + 50)
-		.attr('x2',scaledAllPass)
-		.attr('y2',(scaledAllPass * 2) + 60)
 		
 }
 
