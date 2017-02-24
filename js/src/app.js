23b2a9ee18ac3de955d15f122946c0f17e740e68
ryanbmarx <ryanbmarx@gmail.com>
2017-02-24 13:01:23 -0600

    Logic error required double clicks
    There is no data-checked defined on load, so the buttons needed to be clicked once for the handler to create that attribute. Switching the logic (look for true, not false on data-checked) allowed for the attribute's creation on the first click.

--- js/src/app.js
+++ js/src/app.js
@@ -160,17 +160,17 @@ window.onload = function(){
     // ####   CHECKING/UNCHECKING FILTER BUTTONS
 
     for (var filterButton of document.getElementsByClassName('filter-button')){
-        filterButton.addEventListener('click', e => {
+        filterButton.addEventListener('click', function(e) {
+            console.log(e.target, 'clicked')
             // Toggle the checked data-* attribute on the actual button
-            e.target.dataset.checked = e.target.dataset.checked == "false" ? "true" : "false";
+            e.target.dataset.checked = e.target.dataset.checked == "true" ? "false" : "true";
         })
-        // Also do focus because without it, the buttons required a double click to register the event
-        filterButton.addEventListener('focus', e => {
-            // Toggle the checked data-* attribute on the actual button
-            e.target.dataset.checked = e.target.dataset.checked == "false" ? "true" : "false";
-        })
+        // // Also do focus because without it, the buttons required a double click to register the event
+        // filterButton.addEventListener('focus', e => {
+        //     // Toggle the checked data-* attribute on the actual button
+        //     e.target.dataset.checked = e.target.dataset.checked == "false" ? "true" : "false";
+        // })
     }
-
 
 
     // ####   SUBMIT BUTTONS
